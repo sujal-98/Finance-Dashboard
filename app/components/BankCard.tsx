5344df1f474ac9d8154e82db44aa66527c575f2a
sujal <sujalgupta1412@gmail.com>
2024-11-29 02:18:06 +0530

    removed sentry

--- app/components/BankCard.tsx
+++ app/components/BankCard.tsx
@@ -2,7 +2,7 @@ import { formatAmount } from '../../lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import  Copy from './Copy'
+import  Copy from './copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
 
